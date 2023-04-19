cbc128952c2db1038669cd23bdca7525887f9ec3
Galdino <emersongaldino@hotmail.com>
2023-04-19 17:26:43 -0300

    move calculate to method in transactions

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -3,68 +3,79 @@ import Header from "../components/Header";
 import SearchForm from "../components/SearchForm";
 import Summary from "../components/Summary";
 import TableTransaction from "./Table";
-import { TransactionsContainer } from './styles';
+import { TransactionsContainer } from "./styles";
 
 export function Transactions() {
-    const [search, setSearch] = useState('');
-    const [data, setData] = useState([{
-        id: 1,
-        title: 'Desenvolvimento de Site',
-        price: '12.000',
-        Type: 'Venda',
-        dtInclude: '13/04/2022',
-        InOrOut: 'income'
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState([
+    {
+      id: 1,
+      title: "Desenvolvimento de Site",
+      price: "12.000",
+      Type: "Venda",
+      dtInclude: "13/04/2022",
+      InOrOut: "income",
     },
     {
-        id: 2,
-        title: 'Prestação',
-        price: '6.000',
-        Type: 'Custo',
-        dtInclude: '13/04/2022',
-        InOrOut: 'outcome'
+      id: 2,
+      title: "Prestação",
+      price: "6.000",
+      Type: "Custo",
+      dtInclude: "13/04/2022",
+      InOrOut: "outcome",
     },
     {
-        id: 3,
-        title: 'Salário',
-        price: '11.500',
-        Type: 'Entrada',
-        dtInclude: '17/04/2022',
-        InOrOut: 'income'
+      id: 3,
+      title: "Salário",
+      price: "11.500",
+      Type: "Entrada",
+      dtInclude: "17/04/2022",
+      InOrOut: "income",
     },
     {
-        id: 4,
-        title: 'Carro',
-        price: '1.800',
-        Type: 'Custo',
-        dtInclude: '17/04/2022',
-        InOrOut: 'outcome'
+      id: 4,
+      title: "Carro",
+      price: "1.800",
+      Type: "Custo",
+      dtInclude: "17/04/2022",
+      InOrOut: "outcome",
     },
     {
-        id: 5,
-        title: 'Condominio',
-        price: '675,00',
-        Type: 'Custo',
-        dtInclude: '17/04/2022',
-        InOrOut: 'outcome'
-    }
-    ]);
+      id: 5,
+      title: "Condominio",
+      price: "675,00",
+      Type: "Custo",
+      dtInclude: "17/04/2022",
+      InOrOut: "outcome",
+    },
+  ]);
 
-    const handleDelete = (id: Number) => {
-        data.indexOf(id)
-        setData(data.slice())
-    }
+  const handleDelete = (id: Number) => {
+    data.indexOf(id);
+    setData(data.slice());
+  };
 
-    useEffect(() => { }, [data])
+  useEffect(() => {}, [data]);
 
-    return (
-        <>
-            <Header set={setData} data={data} />
-            <Summary />
+  const values = {
+    entrance: "14.000",
+    out: "7.500",
+    total: "22.000",
+  };
 
-            <TransactionsContainer>
-                <SearchForm set={setSearch} />
-                <TableTransaction get={search} data={data} handleDelete={handleDelete} />
-            </TransactionsContainer>
-        </>
-    )
+  return (
+    <>
+      <Header set={setData} data={data} />
+      <Summary values={values} />
+
+      <TransactionsContainer>
+        <SearchForm set={setSearch} />
+        <TableTransaction
+          get={search}
+          data={data}
+          handleDelete={handleDelete}
+        />
+      </TransactionsContainer>
+    </>
+  );
 }
--- src/pages/components/Summary/index.tsx
+++ src/pages/components/Summary/index.tsx
@@ -1,38 +1,46 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
-import { SummaryContainer, SummaryCard } from './styles';
+import { SummaryContainer, SummaryCard } from "./styles";
+import React from "react";
 
-
-
-import React from 'react';
-
+interface ISummaryValues {
+  entrance: string;
+  out: string;
+  total: string;
+}
+interface IValuesProps {
+  values: ISummaryValues;
+}
 // import { Container } from './styles';
 
-const Summary: React.FC = () => {
-    return (
-        <SummaryContainer>
-            <SummaryCard>
-                <header>
-                    <span>Entradas</span>
-                    <ArrowCircleUp size={32} color="#00b37e" />
-                </header>
-                <strong>R$ 17.400,00</strong>
-            </SummaryCard>
-            <SummaryCard>
-                <header>
-                    <span>Saidas</span>
-                    <ArrowCircleDown size={32} color="#f75a68" />
-                </header>
-                <strong>R$ 17.400,00</strong>
-            </SummaryCard>
-            <SummaryCard variant="green">
-                <header>
-                    <span>Total</span>
-                    <CurrencyDollar size={32} color="#fff" />
-                </header>
-                <strong>R$ 17.400,00</strong>
-            </SummaryCard>
-        </SummaryContainer>
-    )
-}
+const Summary: React.FC<IValuesProps> = (props) => {
+  const { values } = props;
+  return (
+    <SummaryContainer>
+      <SummaryCard>
+        <header>
+          <span>Entradas</span>
+          <ArrowCircleUp size={32} color="#00b37e" />
+        </header>
+        <strong>R$ {parseFloat(values.entrance).toFixed(3)}</strong>
+      </SummaryCard>
+      <SummaryCard>
+        <header>
+          <span>Saidas</span>
+          <ArrowCircleDown size={32} color="#f75a68" />
+        </header>
+        <strong>R$ {parseFloat(values.out).toFixed(3)}</strong>
+      </SummaryCard>
+      <SummaryCard variant="green">
+        <header>
+          <span>Total</span>
+          <CurrencyDollar size={32} color="#fff" />
+        </header>
+        <strong>
+          R$ {(parseFloat(values.entrance) - parseFloat(values.out)).toFixed(3)}
+        </strong>
+      </SummaryCard>
+    </SummaryContainer>
+  );
+};
 
 export default Summary;
