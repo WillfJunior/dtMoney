e7aae68cf56f9dd01781dfd6e541ce79f2323370
EmersonGaldino <emersongaldino@hotmail.com>
2023-05-20 22:47:57 -0300

    ola husky

--- src/pages/components/NewTransactionModal/index.tsx
+++ src/pages/components/NewTransactionModal/index.tsx
@@ -70,14 +70,7 @@ const NewTransactionModal: React.FC<IDataProps> = (props) => {
             onChange={(e) => setPrice(e.target.value)}
             placeholder="Preço"
             required />
-          {/* <Select.Root>
-            <Select.Trigger className="SelectTrigger" aria-label="Food">
-              <Select.Value placeholder="Select a fruit…" />
-              <Select.Icon className="SelectIcon">
-                <ArrowDown />
-              </Select.Icon>
-            </Select.Trigger>
-          </Select.Root> */}
+
           <input
             type="text"
             onChange={(e) => setType(e.target.value)}
